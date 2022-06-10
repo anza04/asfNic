32de0c091505b7ddaecaf759f6ee84cc6a453447
anza04 <64538739+anza04@users.noreply.github.com>
2022-06-11 00:00:29 +0200

    4.0.0

--- js/script.js
+++ js/script.js
@@ -1,44 +1,20 @@
-var biglietto = document.getElementById("biglietto");
-var video = document.getElementById("video");
-var nav = document.getElementById("nav");
-var sottonav = document.getElementById("sottonav");
-var biglietti = document.getElementById("biglietti")
-var aggiungi = document.getElementById("aggiungi")
+var orario = new Date();
 
-var conta = 0;
+document.getElementById("attivazione").innerHTML=orario.getDate()+"/"+(orario.getMonth()+1)+"/"+orario.getFullYear()+" "+(orario.getHours()-1)+":"+orario.getMinutes()+":"+orario.getSeconds();
 
-function Click()
-{
-    conta+=1;
-    console.log(conta);
-    if(conta%2==0)
-    {
-        document.getElementsByTagName("video")[0].pause();
-        video.style.display="none";
-        nav.style.height="7vh";
-        sottonav.style.opacity=1;
-        biglietti.style.opacity=1;
-        nav.style.opacity=1;
-        aggiungi.style.opacity=1;
+window.onload = setInterval(Orologio,1000);
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
     }
-    else
-    {
-        document.getElementsByTagName("video")[0].play();
-        video.style.display="block";
-        nav.style.height="20vh";
-        sottonav.style.opacity=0;
-        biglietti.style.opacity=0;
-        nav.style.opacity=0;
-        aggiungi.style.opacity=0;
+    return i;
     }
-}
-
-function Aggiungi()
+function Orologio()
 {
-    var buffer ="<div class='biglietto-nuovo' id='biglietto' onclick='Click()'><div class='foto'><img src='./img/WhatsApp Image 2022-05-23 at 13.06.36.jpeg' width='70px'></div><div class='testo'><b>";
-    var nome_corsa = prompt("Inserire il nome della corsa");
-    buffer += nome_corsa+"</b><br>";
-    var attivazione = prompt("Inserire giorno e data di attivazione");
-    buffer += attivazione+"</div></div>";
-    biglietti.innerHTML+=buffer;
+    var d = new Date();
+    var date = d.getDate();
+    var ora = addZero(d.getHours());
+    var min = addZero(d.getMinutes());
+    var sec = addZero(d.getSeconds());
+    document.getElementById("orologio").innerHTML=ora+":"+min+":"+sec;
 }
